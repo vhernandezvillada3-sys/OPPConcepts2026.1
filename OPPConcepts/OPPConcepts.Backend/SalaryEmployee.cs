d768fb6b47115e887bb64ac669a408f4391b77da
user <user@DESKTOP-PNR9EFR>
2026-02-24 17:56:45 -0500

    Add SalaryEmployee Class

---
+++ OPPConcepts/OPPConcepts.Backend/SalaryEmployee.cs
@@ -0,0 +1,16 @@
+﻿
+namespace OPPConcepts.Backend
+{
+    public class SalaryEmployee : Employee
+    {
+        public SalaryEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate) :
+            base(id, firstName, lastName, isActive, bornDate, hireDate)
+        {
+        }
+
+        public override decimal GetValueToPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
