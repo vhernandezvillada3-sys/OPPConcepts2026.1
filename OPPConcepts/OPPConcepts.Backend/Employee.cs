23fbaf63e689c88a494cfa9e2d787e4c2dc9eb5d
user <user@DESKTOP-PNR9EFR>
2026-02-24 15:45:23 -0500

    Add employee class

---
+++ OPPConcepts/OPPConcepts.Backend/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPPConcepts.Backend
+{
+    public abstract class Employee
+    {
+        //Constructors
+
+        protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            IsActive = isActive;
+            BornDate = bornDate;
+            HireDate = hireDate;
+        }
+
+
+        //Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public Date BornDate { get; set; } = null!;
+        public Date HireDate { get; set; } = null!;
+
+        //Methods
+        public override string ToString()
+        {
+            return $"{Id}\t{FirstName} {LastName}\n\t" +
+                   $"Value to pay: {GetValueToPay()}";
+        }
+        public abstract decimal GetValueToPay();
+        
+    }
+}
