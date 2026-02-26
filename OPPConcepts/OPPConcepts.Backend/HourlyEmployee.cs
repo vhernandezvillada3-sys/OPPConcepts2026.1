676273624db70fe1f287f69feb8d4de4becab524
user <user@DESKTOP-PNR9EFR>
2026-02-26 16:33:09 -0500

    Add HourlyEmployee

---
+++ OPPConcepts/OPPConcepts.Backend/HourlyEmployee.cs
@@ -0,0 +1,52 @@
+﻿namespace OPPConcepts.Backend
+{
+    public class HourlyEmployee : Employee
+    {
+        //Fields
+        private decimal _hourlyValue;
+        private float _workingHours;
+
+        //Constructors
+        public HourlyEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, decimal hourlyValue, float workingHours) :
+            base(id, firstName, lastName, isActive, bornDate, hireDate)
+        {
+            HourlyValue = hourlyValue;
+            WorkingHours = workingHours;
+        }
+
+        //Properties
+        public decimal HourlyValue 
+        { 
+            get => _hourlyValue; 
+            set => _hourlyValue = ValidateHourlyValue(value); 
+        }
+        public float WorkingHours 
+        { 
+            get => _workingHours; 
+            set => _workingHours = ValidateWorkingHours(value); 
+        }
+        //Methods
+        public override decimal GetValueToPay()=> (decimal)WorkingHours * HourlyValue;
+     
+        override public string ToString() => base.ToString() + $"\n\t" +
+            $"Hourly Value.....: {HourlyValue,20:C2}\n\t" +
+            $"Working Hours....: {WorkingHours,20:N2}";
+
+        private decimal ValidateHourlyValue(decimal hourlyValue)
+        {
+            if (hourlyValue < 10000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hourlyValue), "Hourly value must be greater than or equal to $10,000.");
+            }
+            return hourlyValue;
+        }
+         private float ValidateWorkingHours(float workingHours)
+        {
+            if (workingHours < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workingHours), "Working hours must be greater than or equal to 0.");
+            }
+            return workingHours;
+        }
+    }
+}
--- OPPConcepts/OPPConcepts.Frontend/Program.cs
+++ OPPConcepts/OPPConcepts.Frontend/Program.cs
@@ -2,10 +2,27 @@
 
 try
 {
+
+    var employees = new List<Employee>();
+    decimal payroll = 0;
     var employee1 = new SalaryEmployee(1010, "María", "Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
+    employees.Add(employee1);
     var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1990, 5, 15), new Date(2016, 1, 1), 10395876);
-    Console.WriteLine(employee1);
-    Console.WriteLine(employee2);
+    employees.Add(employee2);
+    var employee3 = new HourlyEmployee(2023, "Ana", "lopez", true, new Date(1995, 5, 15), new Date(2022, 1, 1), 15000, 10);
+    employees.Add(employee3);
+    var employee4 = new HourlyEmployee(2024, "Carlos", "Garcia", true, new Date(1995, 5, 15), new Date(2022, 1, 1), 10000, 160);
+    employees.Add(employee4);
+
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(new string('-', 47));
+        Console.WriteLine(employee);
+        payroll += employee.GetValueToPay();
+
+    }
+    Console.WriteLine(new string('=', 47));
+    Console.WriteLine($"payroll..................: {payroll,20:C2}");
 }
 catch (Exception ex)
 { 
